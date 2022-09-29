a71af44b25266bee769d4f95b06aa35ccee29441
OUAIS MON FRERE <jeralexis77@aol.com>
2022-09-29 16:57:15 +0200

    bug route

--- components/Header-exemple.js
+++ components/Header-exemple.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import { PlusCircleIcon } from '@heroicons/react/outline'
+import { HomeIcon } from '@heroicons/react/solid'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Header() {
   return (
     <div className={styles.header}>
         <nav>
-          <a href="#">Home</a>
+          <Link href="/Works">
+            <a>Home <span className={styles.icon}><PlusCircleIcon/></span></a>
+          </Link>
           <a href="#">Works</a>
           <a href="#">Contact</a>
         </nav>
---
+++ pages/Works.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../components/Header'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
+
+
+export default function Works() {
+  return (
+    <div>
+        <div className={styles.john}>
+        <Header/>
+
+        </div>
+    </div>
+  )
+}
