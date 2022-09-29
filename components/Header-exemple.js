eee72225ead5ac24acab41d80baad5f75de58dfb
OUAIS MON FRERE <jeralexis77@aol.com>
2022-09-29 16:32:32 +0200

    nav

--- components/Header-exemple.js
+++ components/Header-exemple.js
@@ -1,16 +1,14 @@
 import React from 'react'
-import { PlusCircleIcon } from '@heroicons/react/outline'
-import { HomeIcon } from '@heroicons/react/solid'
 import styles from '../styles/Home.module.css'
 
 export default function Header() {
   return (
     <div className={styles.header}>
-        <h1>Header</h1>
-        <div>
-            <HomeIcon/>
-            <PlusCircleIcon/>
-        </div>
+        <nav>
+          <a href="#">Home</a>
+          <a href="#">Works</a>
+          <a href="#">Contact</a>
+        </nav>
     </div>
   )
 }
