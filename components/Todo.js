d08c01128ff579c5670e5e8adc61d447e4fa40e5
OUAIS MON FRERE <jeralexis77@aol.com>
2022-09-30 16:47:47 +0200

    style

--- components/Todo.js
+++ components/Todo.js
@@ -4,10 +4,18 @@ import styles from '../styles/Home.module.css'
 export default function Todo({todo}) {
   return (
     <div className={styles.profils_detail}>
-        <img src={todo.fields.Image[0].url} alt={todo.fields.Image[0].url} />
+      <div className={styles.profils_detail__left}>
+        {
+          todo.fields.Image.map(function(each){
+          return(<img src={each.url}/>)
+          })
+        }
+      </div>
+      <div className={styles.profils_detail__right}>
         <h2>{todo.fields.Name}</h2>
         <strong>{todo.fields.Phone}</strong>
         <p>{todo.fields.Description}</p>
+      </div>
     </div>
   )
 }
